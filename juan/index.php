e86630f4a889acc4e93de5489d0d72d8ff3917a0
Juan Pablo Giacalone <48533111+JuanGiacalone@users.noreply.github.com>
2019-09-17 22:53:37 -0300

    Update index.php

--- juan/index.php
+++ juan/index.php
@@ -7,9 +7,10 @@
 <body>	
 	<div align="center"><a href="login.php">INGRESO AL SISTEMA</a></div>
 	<br>
+	<div align="center"><a href="front.php">INGRESO AL FRONT</a></div>
 	<p>
-        <h1>Materia: Recuperatorio de Interfases y Programacion Visual - 2018</h1>       
-        <h3>Alumno: Fabián A. Quinteros</h3>
+        <h1>INTEGRADOR PROGRAMACION E INTERFACES VISUALES</h1>       
+        <h3>Alumno: GIACALONE JUAN PABLO</h3>
 	</p>
 </body>
 </html>
